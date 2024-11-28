085b663efa4b5682b90d34b54651067dfba614bb
HUI-Space <821627916@qq.com>
2024-11-28 22:19:46 +0800

    init

---
+++ Assets/Collider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Collider : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private async void LoadP()
+    {
+        AssetBundle assetBundle = await Load("");
+        Addressables.LoadAssetAsync<GameObject>("");
+    }
+    
+
+    private async Task<AssetBundle> Load(string path)
+    {
+        TaskCompletionSource<AssetBundle> taskCompletionSource = new TaskCompletionSource<AssetBundle>();
+        AssetBundleCreateRequest assetBundleCreateRequest = AssetBundle.LoadFromFileAsync(path);
+        assetBundleCreateRequest.completed += operation =>
+        {
+            taskCompletionSource.SetResult(assetBundleCreateRequest.assetBundle);
+        };
+        return await taskCompletionSource.Task;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("产生碰撞了");
+        StartCoroutine(WaitAndDestroy());
+    }
+
+    IEnumerator WaitAndDestroy()
+    {
+        Debug.Log("播放特效");
+        yield return new WaitForSeconds(3F);
+        Debug.Log("销毁了");
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/CourseResources/taecgLibrary/ShaderReference/CHANGELOG.md
@@ -0,0 +1,20 @@
+## 更新日志
+
+### v1.0.4
+* 新增#pragma editor_sync_compilation
+* 新增#include
+* 新增#include_with_pragmas
+* 新增AlphaToMask、Conservative、ZClip
+
+### v1.0.3
+* 增加SRPBatcher合批失败的原因.
+
+### v1.0.2
+* 增加Dither算法(2x2,4x4,8x8的数值表).
+
+### v1.0.1
+* 增加4种去色的写法.
+
+### v1.0.0
+* 经过长时间的迭代，首次将插件放置在UnityPackageManager上.
+
